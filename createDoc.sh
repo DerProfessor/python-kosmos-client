3d877fbfa32cd8d8f499e5a08501b624fe79109b
Jan <jan.janssen@dfki.de>
2022-02-01 18:04:45 +0000

    moved html

--- createDoc.sh
+++ createDoc.sh
@@ -1,1 +1,1 @@
-pdoc --html --output-dir doc src/kosmos_client/kosmos.py --force --config show_source_code=False && mv doc/kosmos.html doc/index.html
+pdoc --html --output-dir docs src/kosmos_client/kosmos.py --force --config show_source_code=False && mv docs/kosmos.html docs/index.html
