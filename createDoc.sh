5d6a8f413b79e3525af885a171c47c825a3f6e61
Jan <jan.janssen@dfki.de>
2022-02-01 18:01:08 +0000

    minor update

--- createDoc.sh
+++ createDoc.sh
@@ -1,1 +1,1 @@
-pdoc --html --output-dir doc src/kosmos_client/kosmos.py --force --config show_source_code=False
+pdoc --html --output-dir doc src/kosmos_client/kosmos.py --force --config show_source_code=False && mv doc/kosmos.html doc/index.html
