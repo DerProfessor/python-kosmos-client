0cdbf0fe0dcae1c9293a9e6faf0114da097d45b2
Jan <jan.janssen@dfki.de>
2022-02-08 10:53:42 +0000

    Bump version: 0.8.3 → 0.8.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 setuptools.setup(
     name="kosmos-client",
-    version="0.8.2",
+    version="0.8.3",
     author="Jan Janssen",
     author_email="Jan.Janssen@dfki.de",
     description="Client to connect to the KosmoS Platform",
