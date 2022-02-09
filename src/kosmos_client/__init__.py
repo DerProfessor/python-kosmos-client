3e7b22a05ecaf927ad490b66b44c72a2d12182d3
Jan <jan.janssen@dfki.de>
2022-02-09 10:31:28 +0000

    Bump to 0.9.0

--- src/kosmos_client/__init__.py
+++ src/kosmos_client/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.8.8'
+__version__ = '0.9.0'
 from .kosmos import KosmosClient, KosmosEvent, KosmosDevice, KosmosScope, KosmosLocation, KosmosError, \
      KosmosNotFoundError, KosmosConflictError
